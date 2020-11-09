95cf1af9e47fdfca3eba0e5e36287e4a3edd3a60
RyanGibson292 <72360102+RyanGibson292@users.noreply.github.com>
2020-11-09 12:14:28 +0000

    github

--- Assets/Scripts/EnemyTest.cs
+++ Assets/Scripts/EnemyTest.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyTest : MonoBehaviour
 {
     private int ticksExisted;
     private GameObject target;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D newRigidbody;
     public bool hasWeapon;
     public GameObject weapon;
+
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        newRigidbody = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         ticksExisted++;
@@ -22,8 +22,24 @@ public class EnemyTest : MonoBehaviour
             } else {
                 Vector3 displacement = target.transform.position - transform.position;
                 displacement = displacement.normalized;
-                transform.position += (displacement * 5f * Time.deltaTime);
+                transform.position += (displacement * 2.5f * Time.deltaTime);
             }
+        } else {
+            ApplyNormalMovement();
         }
+    }
+
+    private void ApplyNormalMovement() {
+        newRigidbody.AddForce(new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player")) {
+            SetTarget(other.gameObject);
+        }
+    }
+
+    public void SetTarget(GameObject targetIn) {
+        this.target = targetIn;
     }
 }
---
+++ Assets/Scripts/SafePoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SafePoint : MonoBehaviour {
+    private GameObject capture;
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Enemy") && capture == null) {
+            capture = other.gameObject;
+            EnemyTest enemy = capture.GetComponent<EnemyTest>();
+            enemy.SetTarget(this.gameObject);
+        }
+    }
+}
