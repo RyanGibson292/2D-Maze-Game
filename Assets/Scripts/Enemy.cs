6d8530584b664e6ed86ebc5b25ffc9a6f468cee1
RyanGibson292 <72360102+RyanGibson292@users.noreply.github.com>
2020-11-16 12:16:10 +0000

    Too much

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int ticksExisted;
+    private GameObject target;
+    private Rigidbody2D newRigidbody;
+    public GameObject player;
+
+    void Start() {
+        newRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ticksExisted++;
+        if(target != null) {
+            if(Vector2.Distance(transform.position, target.transform.position) > 20f) {
+                target = null;
+            } else {
+                Vector3 displacement = target.transform.position - transform.position;
+                displacement = displacement.normalized;
+
+                newRigidbody.AddRelativeForce(displacement * 0.25f, ForceMode2D.Force);
+            }
+        } else {
+            ApplyNormalMovement();
+        }
+    }
+
+    private void ApplyNormalMovement() {
+        newRigidbody.AddForce(new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player")) {
+            SetTarget(other.gameObject);
+        }
+    }
+
+    public void SetTarget(GameObject targetIn) {
+        this.target = targetIn;
+    }
+
+    public GameObject GetPlayer() {
+        return this.player;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,60 +1,91 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
 
 public class Player : MonoBehaviour {
     private Rigidbody2D body;
-    private int deathTimer, hostagesSaved, lives = 1, hostageCount, wallPhaseTimer, gameTimer;
+    private int deathTimer, hostagesSaved, lives = 1, hostageCount, wallPhaseTimer;
     public TextMeshPro uiText;
     private Vector3 spawnPoint;
+    private long startTime;
+    private bool dead, won;
 
     void Start() {
         body = GetComponent<Rigidbody2D>();
         spawnPoint = transform.position;
+        startTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
     }
 
     void Update() {
+        TransformText();
+        Vector3 velocity = body.velocity;
+
+        if (uiText.text == null || uiText.text == "") {
+            velocity = ApplyMovement(velocity);
+        } else {
+            if(dead) {
+                deathTimer++;
+                if (deathTimer >= 400) {
+                    deathTimer = 0;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+
+        Vector3 position = transform.position;
+        Vector3[] boundReturn = BoundPlayer(position, velocity);
+
+        transform.position = boundReturn[0];
+        body.velocity = boundReturn[1];
+
+        if(hostagesSaved >= hostageCount) {
+            DisplayWinningScreen();
+        }
+
+        if(wallPhaseTimer > 0) {
+            wallPhaseTimer--;
+        }
+    }
+
+    private void TransformText() {
         Vector3 cameraPos = transform.GetChild(0).transform.position;
         cameraPos.x += 3;
         cameraPos.y += 6;
         cameraPos.z = 0;
         uiText.transform.position = cameraPos;
-        Vector3 velocity = body.velocity;
+    }
 
-        if (uiText.text == null || uiText.text == "") {
-            if (Input.GetKey("left") || Input.GetKey("a")) {
-                body.AddForce(new Vector2(-0.5f, 0.0f));
-            } else if (Input.GetKey("right") || Input.GetKey("d")) {
-                body.AddForce(new Vector2(0.5f, 0.0f));
-            } else {
-                if (velocity.x > 0.5f) {
-                    velocity.x = 0.5f;
-                } else if (velocity.x < -0.5f) {
-                    velocity.x = -0.5f;
-                }
-            }
-
-            if (Input.GetKey("up") || Input.GetKey("w")) {
-                body.AddForce(new Vector2(0.0f, 0.5f));
-            } else if (Input.GetKey("down") || Input.GetKey("s")) {
-                body.AddForce(new Vector2(0.0f, -0.5f));
-            } else {
-                if (velocity.y > 0.5f) {
-                    velocity.y = 0.5f;
-                } else if (velocity.y < -0.5f) {
-                    velocity.y = -0.5f;
-                }
-            }
+    private Vector3 ApplyMovement(Vector3 velocity) {
+        if (Input.GetKey("left") || Input.GetKey("a")) {
+            body.AddForce(new Vector2(-0.5f, 0.0f));
+        } else if (Input.GetKey("right") || Input.GetKey("d")) {
+            body.AddForce(new Vector2(0.5f, 0.0f));
         } else {
-            deathTimer++;
-            if (deathTimer >= 400) {
-                deathTimer = 0;
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if (velocity.x > 0.5f) {
+                velocity.x = 0.5f;
+            } else if (velocity.x < -0.5f) {
+                velocity.x = -0.5f;
+            }
+            Debug.Log(velocity);
+        }
+
+        if (Input.GetKey("up") || Input.GetKey("w")) {
+            body.AddForce(new Vector2(0.0f, 0.5f));
+        } else if (Input.GetKey("down") || Input.GetKey("s")) {
+            body.AddForce(new Vector2(0.0f, -0.5f));
+        } else {
+            if (velocity.y > 0.5f) {
+                velocity.y = 0.5f;
+            } else if (velocity.y < -0.5f) {
+                velocity.y = -0.5f;
             }
         }
 
-        Vector3 position = transform.position;
+        return velocity;
+    }
 
+    private Vector3[] BoundPlayer(Vector3 position, Vector3 velocity) {
         if (position.x > 36f) {
             position.x = 36f;
             velocity.x = 0;
@@ -70,40 +101,64 @@ public class Player : MonoBehaviour {
             position.y = -36f;
             velocity.y = 0;
         }
+        return new Vector3[] {position, velocity};
+    }
 
-        transform.position = position;
-        body.velocity = velocity;
-
-        if(hostagesSaved >= hostageCount) {
-            uiText.text = "You Win!!!\nYou completed it in: " + (gameTimer / 350) + " seconds\n\nPress 'e' to restart.";
+    private void DisplayWinningScreen() {
+        if(!won) {
+            long currentTimeMillis = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            double currentTimeSeconds = (currentTimeMillis - startTime) / 1000;
+            double currentTimeMins = currentTimeSeconds / 60;
+            double currentTimeHours = currentTimeMins / 60;
+            double currentTimeDays = currentTimeHours / 24;
+            double currentTimeWeeks = currentTimeDays / 7;
+            double toUse = currentTimeMillis;
+            string toUseString = "ms";
+            if(currentTimeMillis > 1000) {
+                toUse = currentTimeSeconds; 
+                toUseString = "s";
+            } if(currentTimeSeconds > 60) {
+                toUse = currentTimeMins;
+                toUseString = " mins";
+            } if(currentTimeMins > 60) {
+                toUse = currentTimeHours;
+                toUseString = " hours";
+            } if(currentTimeHours > 24) {
+                toUse = currentTimeDays;
+                toUseString = " days";
+            } if(currentTimeDays > 7) {
+                toUse = currentTimeWeeks;
+                toUseString = " weeks";
+            }
+            uiText.text = "You Win!!!\nYou completed it in: " + toUse + toUseString + ". Poggers!!\n\nPress 'e' to restart.";
+            won = true;
+        } else {
+            body.velocity = Vector3.zero;
             if(Input.GetKey("e")) {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
-
-        if(wallPhaseTimer > 0) {
-            wallPhaseTimer--;
-        }
-
-        if(uiText.text == null || uiText.text == "") {
-            gameTimer++;
-        }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Maze")) {
-            if(wallPhaseTimer <= 0) {
-                lives--;
-                transform.position = this.spawnPoint;
-                if(lives <= 0) {
-                    deathTimer++;
-                    if (uiText != null) {
-                        uiText.text = "You died!";
+        if(uiText != null || uiText.text == "") {
+            if (other.gameObject.CompareTag("Maze")) {
+                if(wallPhaseTimer <= 0) {
+                    lives--;
+                    transform.position = this.spawnPoint;
+                    if(lives <= 0) {
+                        dead = true;
+                        deathTimer++;
+                        if (uiText != null) {
+                            uiText.text = "You died!";
+                        }
                     }
+                } else {
+                    Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
                 }
-            } else {
-                Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
             }
+        } else {
+            Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
     }
 
